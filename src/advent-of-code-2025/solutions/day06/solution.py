161e6d351c727a25953234d2b64d44836bfe970e
Sergio Giraldo <sergio@giraldo.com.br>
2025-12-06 19:49:33 +0100

    chore: commenting choice for not using regex in part 2 ::by sergio giraldo @ 20251206T1949CET, gpg signed

--- src/advent-of-code-2025/solutions/day06/solution.py
+++ src/advent-of-code-2025/solutions/day06/solution.py
@@ -3,6 +3,7 @@
 import re
 import time
 from functools import reduce
+import operator
 
 from ..base.advent import *
 
@@ -15,6 +16,11 @@ class Solution(InputAsLinesSolution):
 
     def parse(self, input):
         numbers = []
+
+        # this works for part 1 but for part 2 i cannot strip the spaces
+        #numbers = [list(col) for col in zip(*[re.findall(r"\d+", row) for row in input[:-1]])]
+        
+        # notice the space in the pattern
         matches = list(re.finditer(r"(\W) +", input[-1] + " "))
 
         for match in matches:
@@ -22,39 +28,37 @@ class Solution(InputAsLinesSolution):
             length = len(match.group(0)) - 1
             segment = [row[start : start + length] for row in input[:-1]]
             numbers.append(segment)
-
-        ops = input[-1].replace(" ", "").replace(" ", "")
-        ops = list(ops)
+        
+        ops = re.findall(r"[+*]", input[-1])
 
         return numbers, ops
 
+    def calculate(self, line, op):
+        numbers = map(int, line)
+
+        if op == "+":
+            return reduce(operator.add, numbers, 0)
+        else:
+            return reduce(operator.mul, numbers, 1)
+
     def doHomework(self, input):
         numbers, ops = self.parse(input)
-
-        def calc(arr, op):
-            numbers = [int(digit) for digit in arr]
-            if op == "+":
-                return sum(numbers)
-            else:  # multiplication
-                return reduce(lambda acc, curr: acc * curr, numbers, 1)
-
         ans1 = 0
         ans2 = 0
 
         for i in range(len(ops)):
             # Part 1: horizontal calculation
-            ans1 += calc(numbers[i], ops[i])
+            ans1 += self.calculate(numbers[i], ops[i])
 
-            # Part 2: vertical calculation (transpose columns)
+            # Part 2: vertical calculation
             length = len(numbers[i][0])
 
             parts = [
                 "".join(numbers[i][row][n] for row in range(len(numbers[i])))
                 for n in range(length)
             ]
-
-            ans2 += calc(parts, ops[i])
-
+            ans2 += self.calculate(parts, ops[i])
+        
         return ans1, ans2
 
     def pt1(self, input):
