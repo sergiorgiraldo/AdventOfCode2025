ab93a0b488703d2b34c9ad410ccce5555b0e9228
Sergio Giraldo <sergio@giraldo.com.br>
2025-12-10 09:01:03 +0100

    feat!: day 10 completed. ::by sergio giraldo @ 20251210T0901CET, gpg signed

--- src/advent-of-code-2025/solutions/day09/solution.py
+++ src/advent-of-code-2025/solutions/day09/solution.py
@@ -1,5 +1,6 @@
 # puzzle prompt: https://adventofcode.com/2025/day/9
 
+import sys
 import time
 
 from ..base.advent import *
@@ -13,23 +14,24 @@ class Point:
     def GetArea(self, other):
         return (abs(self.x - other.x) + 1) * (abs(self.y - other.y) + 1)
 
+
 class Edge:
     def __init__(self, p1, p2):
-        self.horizontal = (p1.y == p2.y)
+        self.horizontal = p1.y == p2.y
 
-        smaller = (p1.x < p2.x) if self.horizontal else (p1.y < p2.y)
-        self.p1, self.p2 = (p1, p2) if smaller else (p2, p1)
+        p1_before = (p1.x < p2.x) if self.horizontal else (p1.y < p2.y)
+        self.p1, self.p2 = (p1, p2) if p1_before else (p2, p1)
 
     # Two edges intersect if:
     # - one is horizontal and the other vertical
     # - the X coordinate of the vertical edge is between the X coordinates of the horizontal edge, and
     # - the Y coordinate of the horizontal edge is between the Y coordinates of the vertical edge.
     #               v vert (AB)
-    #    y      - - A - - - -       
-    #    y      - - | - - - - 
+    #    y      - - A - - - -
+    #    y      - - | - - - -
     #    y      - C ------D -  < horz (CD)
-    #    y      - - | - - - -  
-    #    y      - - B - - - -   
+    #    y      - - | - - - -
+    #    y      - - B - - - -
     #           x x x x x x x
     def Intersects(self, other):
         if self.horizontal == other.horizontal:
@@ -39,10 +41,10 @@ class Edge:
         vertical = other if self.horizontal else self
 
         return (
-            vertical.p1.x > horizontal.p1.x and 
-            vertical.p1.x < horizontal.p2.x and 
-            horizontal.p1.y > vertical.p1.y and 
-            horizontal.p1.y < vertical.p2.y
+            vertical.p1.x > horizontal.p1.x
+            and vertical.p1.x < horizontal.p2.x
+            and horizontal.p1.y > vertical.p1.y
+            and horizontal.p1.y < vertical.p2.y
         )
 
 
@@ -57,6 +59,7 @@ class Polygon:
 
     def Intersects(self, other_edge):
         return any(polygon_edge.Intersects(other_edge) for polygon_edge in self.edges)
+
 
 class Solution(InputAsLinesSolution):
     _year = 2025
@@ -102,26 +105,28 @@ class Solution(InputAsLinesSolution):
                 # ..#XXXXXX#XX.. 5
                 # .........XXX.. 6
                 # .........#X#.. 7
-                # .............. 8               
+                # .............. 8
                 # solution will be from rows 3/5
-                # but get reds (11,1) and (2,5), it will be a bigger rectangle. But red (7,1) will intercept, in other 
+                # but get reds (11,1) and (2,5), it will be a bigger rectangle. But red (7,1) will intercept, in other
                 # words, there are tiles not red/green
                 # but .... edges that share a vertex are considered to intersect,
                 # which is guaranteed to happen since all the rectangle's vertices are also vertices of the polygon.
                 # hack: shrink the rectangle for intersection testing
+                x1 = min(p1.x, p2.x) + 0.001
+                x2 = max(p1.x, p2.x) - 0.001
+                y1 = min(p1.y, p2.y) + 0.001
+                y2 = max(p1.y, p2.y) - 0.001
 
-                x1 = min(p1.x, p2.x) + 0.5
-                x2 = max(p1.x, p2.x) - 0.5
-                y1 = min(p1.y, p2.y) + 0.5
-                y2 = max(p1.y, p2.y) - 0.5
+                rectangle = Polygon(
+                    [Point(x1, y1), Point(x2, y1), Point(x2, y2), Point(x1, y2)]
+                )
 
-                rectangle = Polygon([Point(x1, y1), Point(x2, y1), Point(x2, y2), Point(x1, y2)])
-
-                # Rectangle is fully inside polygon; update best answer for part 2 if it's larger
-                if all(not polygon.Intersects(edge) for edge in rectangle.edges):
+                if any(polygon.Intersects(edge) for edge in rectangle.edges):
+                    continue
+                else:  # Rectangle is fully inside polygon; update best answer for part 2 if it's larger
                     red_and_green = max(red_and_green, area)
 
-        return [red_and_any, red_and_green]
+        return red_and_any, red_and_green
 
     def pt1(self, input):
         self.debug(input)
